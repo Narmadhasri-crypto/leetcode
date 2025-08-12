85e56de526c7f3a8635f03b794de3468075c9dae
Narmadhasri-crypto <narmadhasri77@gmail.com>
2025-08-12 19:03:14 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (85.81%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(root == nullptr)return true;
+        return isMirror(root->left, root->right);
+        }
+        bool isMirror(TreeNode* left, TreeNode* right){
+            if(left == nullptr && right == nullptr)return true;
+            if(left == nullptr || right == nullptr)return false;
+            return (left->val == right->val)
+            && isMirror(left->left, right->right)
+            && isMirror(left->right, right->left);
+        }
+};
